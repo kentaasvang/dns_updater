665f90a99f93baa52504658ba5653c5e9eeb3707
Kent Martin Åsvang <github.com@asvang.no>
2024-04-28 20:36:51 +0200

    added simple_cloudflare_api

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
-use serde_json::*;
-use std::{fs, io::Read};
+use simple_cloudflare_api::list_zones;
+use std::fs;
 
 #[derive(Deserialize, Debug)]
 struct Settings {
@@ -10,15 +10,21 @@ struct Settings {
 fn get_settings(path: String) -> Settings {
     let content = fs::read_to_string(path).expect("settings file not found");
 
-    let settings: Settings = serde_json::from_str(&content).expect("Couldn't deserialize settings file");
+    let settings: Settings =
+        serde_json::from_str(&content).expect("Couldn't deserialize settings file");
     settings
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // Read config
     let config_path = String::from("settings.json");
     let config = get_settings(config_path);
-    println!("{:?}", config);
+
+    // Get all A-records from cloudflare
+    let zones = list_zones(config.cf_api_key).await;
+
+    println!("{:?}", zones);
 
     // store domains in db
     // store in file temporary
@@ -28,7 +34,6 @@ fn main() {
     //fs::create_dir_all("/opt/dns_updater").expect("failed to create dns_updater folder");
     // let data_file = fs::File::create_new("/opt/dns_updater/").expect("path already exists");
 
-    // Get all A-records from cloudflare
     // Store records in database
     // Sleep for N seconds
     // Get public IP address
